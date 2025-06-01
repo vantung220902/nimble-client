f85620833824381b3ac9c83ee5354923efb071dc
Tung Nguyen <tung@nimblehq.co>
2025-06-01 11:57:26 +0700

    feat: add github workflows

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,12 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_BUILD_MODE: string;
   readonly VITE_COOKIE_DOMAIN: string;
   readonly VITE_API_URL: string;
-  readonly VITE_ASSETS_URL: string;
   readonly VITE_WEB_URL: string;
-  readonly VITE_SHOW_MOCK: string;
-  readonly VITE_GOOGLE_CLIENT_ID: string;
-  readonly VITE_GOOGLE_MAP_API_KEY: string;
-  readonly VITE_SHARE_URL: string;
 }
 
 interface ImportMeta {
